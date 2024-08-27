bd3df612ec31756f9aefc0c42b455a216f58cd1d
ciciica <144987656+ciciica@users.noreply.github.com>
2024-08-27 07:56:55 +0700

    Add files via upload

---
+++ bab4soalll1/src/bab4soalll1/Bab4soalll1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soalll1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bab4soalll1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("Hasil Pertambahan: " + (angka1 + angka2 ));
+        System.out.println("Hasil Pengurangan: " + (angka1 - angka2 ));
+        System.out.println("Hasil Perkalian  : " + (angka1 * angka2 ));
+        System.out.println("Hasil Pembagian  : " + (angka1 / angka2 ));
+        
+    }
+    
+}
