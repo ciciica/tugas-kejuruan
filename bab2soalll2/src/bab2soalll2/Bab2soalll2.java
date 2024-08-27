6f697448b4cea0cb34489fc9385a3bf072fd0dc1
ciciica <144987656+ciciica@users.noreply.github.com>
2024-08-27 07:53:59 +0700

    Add files via upload

---
+++ bab2soalll2/src/bab2soalll2/Bab2soalll2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soalll2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bab2soalll2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan = "ayam geprek";
+        String minuman = "jus jambu";
+        
+        System.out.println("makanan favorit saya : " + makanan);
+        System.out.println("minuman favorit saya : " + minuman);
+    }
+    
+}
