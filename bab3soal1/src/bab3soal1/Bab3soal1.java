9df67875e95287b9bb0b6808bf48788122aeec72
ciciica <144987656+ciciica@users.noreply.github.com>
2024-08-27 07:54:55 +0700

    Add files via upload

---
+++ bab3soal1/src/bab3soal1/Bab3soal1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bab3soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Var1 = 9;
+        float Var2 = 8.99f;
+        char Var3 = 'A';
+        boolean Var4 = false;
+        String Var5 = "Hallo World";
+        
+        System.out.println(Var1);
+        System.out.println(Var2);
+        System.out.println(Var3);
+        System.out.println(Var4);
+        System.out.println(Var5);
+    }
+    
+}
