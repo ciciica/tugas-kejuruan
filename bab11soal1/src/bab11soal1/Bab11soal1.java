25a248c4ab42923211ee601d5b1cde3d5d963b69
ciciica <144987656+ciciica@users.noreply.github.com>
2024-08-29 12:16:11 +0700

    Add files via upload

---
+++ bab11soal1/src/bab11soal1/Bab11soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab11soal1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bab11soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Karyawan karyawan1 = new Karyawan("1111", "Aisyaa", "A", "Manager", 5000000);
+        
+        // Menampilkan informasi karyawan
+        karyawan1.tampilkanInfo();
+    }
+}
+   
